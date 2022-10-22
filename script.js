8ed0cf25ed147d17f64aa8fa0cf65901ded1a9b8
Soumya <95014925+soumyru@users.noreply.github.com>
2022-10-22 16:51:38 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function Func() {
-    var x = document.getElementById("topNavbar");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
-  }
