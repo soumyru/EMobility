901f981877abd4e434824a12ce7430e4d398f08f
Soumya <soumyashru11@gmail.com>
2022-10-28 11:54:00 +0530

    update

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from "./Components/Navbar";
+import About from "./Components/About";
+
+function App() {
+  return (
+    <>
+<Navbar title="Electron Mobility"/>
+<About/>
+    </>
+  );
+}
+
+export default App;
